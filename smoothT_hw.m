0899d53a5bf5b5275a89f3e0ea8074c663f780a4
조문선 <80917789+chom5621@users.noreply.github.com>
2022-10-26 00:02:23 +0900

    Update smoothT_hw.m

--- smoothT_hw.m
+++ smoothT_hw.m
@@ -1,12 +1,13 @@
 % 알파벳 좌표 찍기
-x_E = [];
-y_E = [];
+x_T = [];
+y_T = [];
 
-figure; plot(x_E, y_E);
+figure; plot(x_T, y_T);
 
-[x_E, y_E] = subd_image(x_E,y_E)
-[x_E, y_E] = subd_image(x_E,y_E) % 점점 매끄러운 함수 출력
-figure; plot(x_E,y_E,'-b')
+[x_T, y_T] = subd_image(x_T,y_T)
+[x_T2, y_T2] = subd_image(x_T,y_T) % 점점 매끄러운 함수 출력
+figure; plot(x_T,y_T,'-b')
+figure; plot(x_T2,y_T2,'-b')
 
 function [t_subd] = subdivision(t)
 
@@ -14,13 +15,14 @@ t1 = [t(end-1), t(1:end), t(2)];
 n = length(t);
 t_subd = zeros(1,2*n-1);
 
-for t = 2:n+1
-    t_subd(2*t-3) = t1(i);
+for i = 2:n+1
+    t_subd(2*i-3) = t1(i);
 end
 
-for t = 2:n+1
-    t_subd(2*t-1) = 
+for i = 2:n
+    t_subd(2*i-1) = mask*[t1(i-1); t1(i); t1(i+1); t1(i+2)];
 end
 
+figure; plot(x, y, '-bo', n, t_subd, '-rx');
 end
 
