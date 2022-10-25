cef52a317596219810b63e2f355e3950468a0355
조문선 <80917789+chom5621@users.noreply.github.com>
2022-10-25 23:59:35 +0900

    Update smoothE_1017.m

--- smoothE_1017.m
+++ smoothE_1017.m
@@ -1,5 +1,5 @@
-x_E = [];
-y_E = [];
+x_E = [11, 64, 64, 30, 30, 60, 60, 30, 30, 65, 65, 11, 11 ];
+y_E = [-72, -72, -86, -86, -110, -110, -125, -130, -155, -155, -166, -165, -72];
 
 figure; plot(x_E, y_E);
 
@@ -13,13 +13,14 @@ t1 = [t(end-1), t(1:end), t(2)];
 n = length(t);
 t_subd = zeros(1,2*n-1);
 
-for t = 2:n+1
-    t_subd(2*t-3) = t1(i);
+for i = 2:n+1
+    t_subd(2*i-3) = t1(i);
 end
 
-for t = 2:n+1
-    t_subd(2*t-1) = 
+for i = 2:n
+    t_subd(2*i-1) = mask*[t1(i-1); t1(i); t1(i+1); t1(i+2)];
 end
 
+figure; plot(x, y, '-bo', n, t_subd, '-rx');
 end
 
